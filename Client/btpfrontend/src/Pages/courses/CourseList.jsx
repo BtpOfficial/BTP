0f585d4c1eb1c7b075c9029d979467e1c2b97b35
Pragya Agarwal <gauravagrawal1202@gmail.com>
2024-04-04 00:56:53 +0530

    fetching data

--- Client/btpfrontend/src/Pages/courses/CourseList.jsx
+++ Client/btpfrontend/src/Pages/courses/CourseList.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 function CourseList(props) {
+    const [data, setData] = useState([]);
+    const { subjectId } = useParams();
+    const getdata = async () => {
+        try {
+            const res = await fetch(`http://localhost:3001/${subjectId}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            const fetchedData = await res.json();
+            setData(fetchedData);
+        } catch (error) {
+            console.error('Data not found', error.message);
+        }
+    };
+    useEffect(() => {
+        getdata();
+    }, []);
     return (
         <>
-            <div>
-                asdf
-            </div>
+
         </>
     );
 }
