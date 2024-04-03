022430ebc14cf8a2e7a1cd480c7a21743fa8b184
Raghav Agarwal <117740164+2929raghav@users.noreply.github.com>
2024-04-03 23:33:45 +0530

    removing verify token

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,11 +14,11 @@ const router = express.Router();
 
 router.post("/:subjectId/:courseId/:unitId/:topicId/read", verifyToken, markComplete);
 router.post("/:subjectId/:courseId/:quizId/quiz", verifyToken, markCompleteQuestion);
-router.post("/:subjectId/:courseId/:unitId/add", verifyToken, addTopic);
-router.post("/:subjectId/:courseId/add", verifyToken, addUnit);
-router.post("/:subjectId/:courseId/add/quiz", verifyToken, addQuiz);
-router.post("/:subjectId/add", verifyToken, addCourse);
-router.post("/add", verifyToken, addSubject);
+router.post("/:subjectId/:courseId/:unitId/add", addTopic);
+router.post("/:subjectId/:courseId/add",  addUnit);
+router.post("/:subjectId/:courseId/add/quiz", addQuiz);
+router.post("/:subjectId/add",addCourse);
+router.post("/add", addSubject);
 
 
 export default router;
